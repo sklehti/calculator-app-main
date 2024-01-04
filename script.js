4cbf4c5fcfd118a49a89a8ed4d57a79cccba1d1a
Saara Lehtinen <saaralehtinen77@gmail.com>
2024-01-04 13:35:31 +0200

    bug fixing

--- script.js
+++ script.js
@@ -239,7 +239,7 @@ function handleCalculatorBtn(button) {
   const btnValue = button.value;
   const resultDiv = document.querySelector(".result");
 
-  var snd = new Audio("sounds/click-btn.mp3");
+  var snd = new Audio("/sounds/click-btn.mp3");
   snd.play();
 
   if (!isNaN(btnValue) || btnValue === ".") {
